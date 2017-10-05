966d62adbe985ed9fb8f85b8398b34f9c5754e3b
dave234 <daveoneill234@gmail.com>
2017-10-05 15:58:23 -0700

    Initial Commit

---
+++ AKFilePlayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AKFilePlayer
+//
+//  Created by dave on 10/5/17.
+//  Copyright © 2017 AudioKit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AKFilePlayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AKFilePlayer
+//
+//  Created by dave on 10/5/17.
+//  Copyright © 2017 AudioKit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
